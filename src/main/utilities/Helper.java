b60e4c9be7528f77e07a06fea9b976e16c9f3547
iamqatech <tech.prashants@gmail.com>
2026-08-12 18:40:36 +0530

    Second commit. Check

--- src/main/utilities/Helper.java
+++ src/main/utilities/Helper.java
@@ -1,17 +1,7 @@
 package utilities;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.JsonObject;
-import com.networknt.schema.SchemaIdValidator;
-import org.json.JSONObject;
-
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
 import java.util.Properties;
 
 public class Helper {
@@ -27,7 +17,6 @@ public class Helper {
         }
 
     }
-
 
 
 }
