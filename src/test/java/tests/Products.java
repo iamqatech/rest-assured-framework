1bdaf1b18e1f81803092ef9598a8e6234d686ee8
iamqatech <tech.prashants@gmail.com>
2026-08-13 19:29:00 +0530

    Updated the structure and added testng.xml file to integrate it with Jenkins.

--- src/test/java/tests/Products.java
+++ src/test/java/tests/Products.java
@@ -4,13 +4,13 @@ import io.restassured.module.jsv.JsonSchemaValidator;
 import org.apache.http.HttpStatus;
 import org.testng.annotations.Test;
 import utilities.SchemaPath;
+import utilities.Helper;
 
 import static io.restassured.RestAssured.given;
-import static utilities.Helper.getProperty;
 
 public class Products {
 
-    public static String baseUrl = getProperty("baseUrl");
+    public String baseUrl = Helper.getProperty("baseUrl");
 
     @Test(priority = 2)
     public void getProduct() {
@@ -28,7 +28,7 @@ public class Products {
                 .header("Accept", "application/json")
                 .header("Content-Type", "application/json")
                 .when().get("/Products")
-                .then().log().body()
+                .then()
                 .statusCode(HttpStatus.SC_OK)
                 .assertThat().body(JsonSchemaValidator.matchesJsonSchemaInClasspath(SchemaPath.PRODUCTS_SCHEMA.getPath()));
 
